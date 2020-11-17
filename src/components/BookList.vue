256412f4fdc2150f627b4e57f78db3aac0a56af6
Jane.FanIT <Jane.FanIT@homecreditcfc.cn>
2020-11-17 11:14:12 +0800

    Completed module

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,20 +1,18 @@
 <template>
     <div>
-        <h1>{{ title }}<h1>
+        <h1>{{ title }}</h1>
     </div>
 </template>
 <script>
 export default {
-    name: BookList,
-    data(): {
+    name: 'BookList',
+    data(){
         return{
-        title: 'All Books'
-    }
+            title: 'All Books'
+        }
     }
 }
 </script>
-
-
 <style scoped>
 h1,h2 {
     font-weight: normal;
